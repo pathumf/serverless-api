5c1875700ae9b1dc9a6f3eed143412858438d6fd
pathumf <madushanwf@gmail.com>
2022-07-06 14:04:15 +0100

    TASK02 - lambda and unit test

---
+++ cmd/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		expect  string
+		err     error
+	}{
+		{
+			request: events.APIGatewayProxyRequest{Body: "World"},
+			expect:  "Hello World",
+			err:     nil,
+		},
+		{
+			request: events.APIGatewayProxyRequest{Body: ""},
+			expect:  "",
+			err:     errors.New("no name was provided in the HTTP body"),
+		},
+	}
+
+	for _, test := range tests {
+		response, err := Handler(test.request)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.expect, response.Body)
+	}
+
+}
